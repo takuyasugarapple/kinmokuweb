6bf9b5b6c5e682beade1aa3aa3b749df042f69cd
takuyasugarapple <takuyasugarapple@gmail.com>
2019-05-28 03:00:05 +0900

    Update .DS_Store
    一旦コミット

--- js/main.js
+++ js/main.js
@@ -1,29 +1,78 @@
-$(function () {
-  var $win = $(window),
-    $header = $('header'),
-    headerHeight = $header.outerHeight(),
-    startPos = 0;
-
-  $win.on('load scroll', function () {
-    var value = $(this).scrollTop();
-    if (value > startPos && value > headerHeight) {
-      $header.css('top', '-' + headerHeight *2 + 'px');
-    } else {
-      $header.css('top', '0');
-    }
-    startPos = value;
-  });
-
-  $(function(){
-    $(window).scroll(function (){
-        $('.fadein').each(function(){
-            var elemPos = $(this).offset().top;
-            var scroll = $(window).scrollTop();
-            var windowHeight = $(window).height();
-            if (scroll > elemPos - windowHeight + 200){
-                $(this).addClass('scrollin');
+$(document).ready(function () {
+    $('.textbox_name').blur(function () {
+            if ($(this).val().length === 0) {
+                $('.label_name').removeClass("focus");
+            } else {
+                returns;
             }
+        })
+        .focus(function () {
+            $('.label_name').addClass("focus")
+        });
+
+    $('.textbox_mail').blur(function () {
+            if ($(this).val().length === 0) {
+                $('.label_mail').removeClass("focus");
+            } else {
+                returns;
+            }
+        })
+        .focus(function () {
+            $('.label_mail').addClass("focus")
+        });
+
+    $('.textbox_pass').blur(function () {
+            if ($(this).val().length === 0) {
+                $('.label_pass').removeClass("focus");
+            } else {
+                returns;
+            }
+        })
+        .focus(function () {
+            $('.label_pass').addClass("focus")
+        });
+
+    $('.textbox_repass').blur(function () {
+            if ($(this).val().length === 0) {
+                $('.label_repass').removeClass("focus");
+            } else {
+                returns;
+            }
+        })
+        .focus(function () {
+            $('.label_repass').addClass("focus")
+        });
+
+
+
+    $('#input1').val(location.href);
+
+    $(function () {
+        $('.share_btn').click(function () {
+            $('#input1').focus();
+            $('#input1').select();
+
+            document.execCommand('copy');
         });
     });
-});
+
+    var $lightbox = $('#lightbox');
+    $('[data-target="#lightbox"]').on('click', function (event) {
+        var $img = $(this).find('img'),
+            src = $img.attr('src'),
+            alt = $img.attr('alt');
+        $lightbox.find('img').attr('src', src);
+        $lightbox.find('img').attr('alt', alt);
+        $lightbox.find('img').css(css);
+    });
+    $lightbox.on('shown.bs.modal', function (e) {
+        var $img = $lightbox.find('img');
+        // $lightbox.find('.modal-dialog').css({
+        //     'width': $img.width()
+        // });
+        $lightbox.find('.close').removeClass('hidden');
+    });
+
+    
+
 });
